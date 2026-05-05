fadde675ebe051364a5a3b7214b6284e205ddb93
Redinferno1736 <code.pranavdp@gmail.com>
2026-05-05 13:52:13 +0530

    bug fix 5

--- chat.py
+++ chat.py
@@ -348,5 +348,5 @@ def find_hospitals():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
